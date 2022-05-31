075662e8da470cba93f44dafb3cd389baf97390e
Hannah Brown <hannah.j.brown@hotmail.co.uk>
2022-05-31 12:53:11 +0100

    Practice using styled components

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,10 @@
+// import React from "react";
 import styled from 'styled-components';
 
+// import styles from './Button.css';
+
 const Button = styled.button`
+    width: 100%;
     font: inherit;
     padding: 0.5rem 1.5rem;
     border: 1px solid #8b005d;
@@ -8,6 +12,11 @@ const Button = styled.button`
     background: #8b005d;
     box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
     cursor: pointer;
+
+
+    @media (min-width: 768px) {
+      width: auto;
+    }
 
   &:focus {
     outline: none;
@@ -20,5 +29,13 @@ const Button = styled.button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 `;
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className={styles.button} onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
